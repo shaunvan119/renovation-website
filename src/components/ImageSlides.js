a79461a8032ef35035c18b76d70b57b4a7a90b9f
shaunvan119 <shaunvan@live.com.au>
2023-03-14 09:50:42 +1100

    cnahged image slide containers

--- src/components/ImageSlides.js
+++ src/components/ImageSlides.js
@@ -4,11 +4,11 @@ import Image1 from '../assets/image1.png';
 import Image2 from '../assets/image2.png';
 import Image3 from '../assets/image3.png';
 
-
-const images = [  Image1,  Image2,  Image3];
+const images = [Image1, Image2, Image3];
 
 const ImageSlides = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -17,20 +17,22 @@ const ImageSlides = () => {
     return () => clearInterval(intervalId);
   }, [activeIndex]);
 
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+
   return (
-    <div className="slides-container">
+    <div className={`slides-container ${isLoaded ? 'loaded' : ''}`}>
       <div className="image-slides">
         <div className="image-slides-wrapper" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
           {images.map((image, index) => (
-            <img key={index} src={image} alt={`Slide ${index + 1}`} className="image-slide" />
+            <img key={index} src={image} alt={`Slide ${index + 1}`} className="image-slide" onLoad={handleImageLoad} />
           ))}
         </div>
       </div>
-   <div className="heading-container">
-  <h1 className="heading">Melbourne home <br/>renovation experts.</h1>
-  
-</div>
-
+      <div className="heading-container">
+        <h1 className="heading">Melbourne home <br />renovation <span className="color__change">experts.</span></h1>
+      </div>
     </div>
   );
 };
