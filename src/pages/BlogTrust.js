9c015b82194649fb195a66d64a77c52b43cdd9ed
shaunvan119 <shaunvan@live.com.au>
2023-03-30 17:13:27 +1100

    test terminal

--- src/pages/BlogTrust.js
+++ src/pages/BlogTrust.js
@@ -10,7 +10,6 @@ const BlogTrust = () => {
    <div className="blog-main-wrapper">
    <Header/>
     <div className="blog-container-pages">
-
     <div className="heading-content">
   <div className="heading-overlay">
     <h1 className="heading">Melbourne home <br />renovation <span className="color__change"><br/>experts.</span><br/><AiOutlineMinus /></h1>
@@ -19,22 +18,16 @@ const BlogTrust = () => {
     </div>
   </div>  
   </div>
-
   <div className="flex-blog-section">
       <h2 className="blog-pages-heading">Renovating Your Home: <span className="blog-pages-heading-change">Renovating your home can be an exciting experience, but finding builders you can trust can be a daunting task.</span></h2>
-
       <h3 className="blog-pages-h3"><span className="color-change-steps">Step 1:</span> Research</h3>
       <p className="blog-pages-p">The first step in finding builders you can trust is to conduct thorough research. Start by asking friends, family, and colleagues for recommendations. You can also check online directories and review websites to see what other customers are saying about builders in your area.</p>
-
       <h3 className="blog-pages-h3"><span className="color-change-steps">Step 2:</span> Check for Licensing and Insurance</h3>
       <p className="blog-pages-p">Once you have a list of potential builders, it's essential to check their licensing and insurance status. A reputable builder should be licensed and insured to protect you from any liability if an accident occurs during the renovation. You can check a builder's licensing status on your state's licensing board website and ask for proof of insurance.</p>
-
       <h3 className="blog-pages-h3"><span className="color-change-steps">Step 3:</span> Check for Experience and Qualifications</h3>
       <p className="blog-pages-p">Experience and qualifications are critical when choosing a builder. Look for builders who have experience in the type of renovation you are planning and have a track record of delivering high-quality work. Check for any professional certifications or memberships to industry associations, which can indicate a builder's level of expertise.</p>
-
       <h3 className="blog-pages-h3"><span className="color-change-steps">Step 4:</span> Ask for References</h3>
       <p className="blog-pages-p">Before hiring a builder, ask for references from past clients. A reputable builder should be willing to provide references and examples of their previous work. You can also ask to visit a completed project to see the quality of their work for yourself.</p>
-
       <h3 className="blog-pages-h3"><span className="color-change-steps">Step 6:</span> Trust Your Instincts</h3>
       <p className="blog-pages-p">Finally, trust your instincts when selecting a builder. If something seems off or if you don't feel comfortable with a builder, it's okay to look for other options. You should feel confident that the builder you choose is the right one for your project.</p>
 
